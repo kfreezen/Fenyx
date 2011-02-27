530105bf3caf1bfe809ce485eec735b344323a9e
Kent Friesen <kfreezen@gmail.com>
2011-02-26 16:09:37 -0800

    First headers commit

---
+++ header/stdint.h
@@ -0,0 +1,13 @@
+#ifndef STDINT_H
+#define STDINT_H
+
+typedef unsigned u32;
+typedef int i32;
+
+typedef unsigned short u16;
+typedef short i16;
+
+typedef unsigned char u8;
+typedef char i8;
+
+#endif // STDINT_H
